2252a459ac96aa4a3105e9899e835b5781aeb5c3
yinyangprime <isszimatsyukri@gmail.com>
2025-07-06 01:55:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,27 @@
 import asyncio
 from telegram import Update
-from telegram.ext import Application, CommandHandler, CallbackContext
+from telegram.ext import Application, CommandHandler, ContextTypes
 from carparktool import CPMNuker
-
-if __name__ == '__main__':
-    nuker = CPMNuker()
-    nuker.start()  # atau method utama awak
 
 TOKEN = "7370949688:AAHfL92Iowqg22K-Tu4GG-xNWO6RyZhHOgk"
 
-async def start(update: Update, context: CallbackContext):
+nuker = CPMNuker()
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Hello! I'm your bot.")
 
 async def main():
     app = Application.builder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
-    
+
     print("Bot is running...")
     await app.run_polling()
 
-if __name__ == "__main__":
+if _name_ == "_main_":
     try:
         loop = asyncio.get_running_loop()
     except RuntimeError:
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
 
-    loop.run_until_complete(main())  # Use this instead of asyncio.run()
+    loop.run_until_complete(main())
